4e94d380d60b824da3918b22348e7963a0eabfa1
hansrajathole <hansrajathole1@gmail.com>
2025-05-20 14:19:33 +0530

    Refactor App component to use ToggleTheme component and implement custom useToggle hook

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,20 +1,9 @@
-import React, { useState } from 'react'
-
-function useToggle() {
-  let initialValue = false
-  const [value, setValue] = useState(initialValue);
-  const toggle = () => setValue((prev) => !prev);
-  return [value, toggle];
-}
-
-const {isToggled, toggle} = useToggle()
-
+import React from 'react'
+import ToggleTheme from './components/ToggleTheme'
 const App = () => {
   return (
     <div>
-      <h1>Custom Hook</h1>
-      <button onClick={toggle}>Toggle</button>
-      <p>{isToggled ? 'ON' : 'OFF'}</p>
+      <ToggleTheme />
     </div>
   )
 }
